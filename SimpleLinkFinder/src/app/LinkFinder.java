3d31d0de3e2a27487e2139337517faea135cdfce
ZFlem96 <zackzacharyfleming@gmail.com>
2016-06-29 14:45:23 -0600

    New Commit!

---
+++ SimpleLinkFinder/src/app/LinkFinder.java
@@ -0,0 +1,18 @@
+package app;
+
+import java.io.InputStream;
+import java.util.Iterator;
+
+public class LinkFinder {
+	   public void processPage(InputStream in) {
+
+	     }
+
+	     public Iterator<String> getLinks() {
+	    	 return null;
+	     }
+	
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ SimpleLinkFinder/src/app/LinkFinderTester.java
@@ -0,0 +1,16 @@
+package app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LinkFinderTester {
+
+	@Test
+	public void test() {
+		LinkFinder test = new LinkFinder();
+//		String result1 = test.processPage(in);
+//		String result2 = test.getLinks();
+	}
+
+}
