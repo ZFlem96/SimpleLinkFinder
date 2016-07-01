94a53dae44e79a41420d78b8e48ee3cf0b7948e7
ZFlem96 <zackzacharyfleming@gmail.com>
2016-07-01 14:54:31 -0600

    end of class 7/1/2016

--- SimpleLinkFinder/src/app/LinkFinder.java
+++ SimpleLinkFinder/src/app/LinkFinder.java
@@ -16,132 +16,83 @@ import java.util.regex.Pattern;
 import org.junit.Test;
 
 public class LinkFinder {
-	private Pattern w;
-	private Matcher d;
+	private String pattern = "<[aA]\\s+[hH][rR][eE][fF].*=.*\"([^\\s]+)\".*>";
 	private String line = "";
 	public static ArrayList<String> allLinks = new ArrayList<String>();
 	public static ArrayList<String> linesInHTML = new ArrayList<String>();
 	public static ArrayList<String> linksInHTML = new ArrayList<String>();
 	public static ArrayList<String> linksinResults = new ArrayList<String>();
-	
+
 	public void processPage(InputStream in) throws IOException {
-		
+
 		System.out.println("In the method");
 		int s = 1;
-		   InputStreamReader readThis = new InputStreamReader(in);
-		   BufferedReader read = new BufferedReader(readThis);
-		  try{ 
-		   
-			   while((line = read.readLine())!= null){
-				   System.out.println(line);
-				   linesInHTML.add(line);
-			   }
-		   }
-		   catch(Exception e){
-			   System.out.println("Nothing to print");
-		   }
-		   System.out.println("--------------------");
-		   FileReader file = new FileReader("Results.txt");
-	 		BufferedReader read1 = new BufferedReader(file);
-	 		String txt = "";
-	 		String txt1 = read1.readLine();
-	 		int i = 0;
-	 		
-	 		while(txt1 != null){
-	 			txt+="\n"+txt1;
-	 			if(i != 0){
-	 				linksinResults = new ArrayList<String>();
-	 				linksinResults.add((i+1)+". "+txt);
-	 			}
-	 			txt1=read1.readLine();
-	 			i++;
-	 			
-	 		}
-	 		System.out.println(linksinResults);
-	     }
-	
-	@Test
- 	public void test() {
- 		
-     }
-     public void finalLinkTest(String linkx ){
-    	 String patternx = "xx.*xx";
- 		Pattern p = Pattern.compile(patternx);
- 		Matcher m = p.matcher(linkx);
- 		boolean matches = m.matches();
- 		if(matches){
- 			allLinks.add(linkx);
- 		}
-     }
-     public void htmlLinksPattern(String lin){
-    	 String pattern = ".*[href].*([abcdesfghijklmnopqrstuvwxyz-/].*html).*";
-    	 Pattern p = Pattern.compile(pattern.toLowerCase());
-  		Matcher m = p.matcher(lin);
-  		boolean matches = m.matches();
-  		if(matches){
-  			linesInHTML.add(m.group(0));
-  		}
-     }
-	
-	     public Iterator<String> getLinks() throws FileNotFoundException,  IOException{
-	    	
-//	    	Iterator<String> results = new Iterator<String>() {
-//				
-//				@Override
-//				public String next() {
-//					// TODO Auto-generated method stub
-//					return null;
-//				}
-//				
-//				@Override
-//				public boolean hasNext() {
-//					// TODO Auto-generated method stub
-//					return false;
-//				}
-//			};
-//	    			(Iterator<>)txt;
-	    	 return null;
-	     }
-	     
+		InputStreamReader readThis = new InputStreamReader(in);
+		BufferedReader read = new BufferedReader(readThis);
+		try {
+
+			while ((line = read.readLine()) != null) {
+				System.out.println(line);
+				linesInHTML.add(line);
+			}
+		} catch (Exception e) {
+			System.out.println("Nothing to print");
+		}
+		System.out.println("--------------------");
+		FileReader file = new FileReader("Results.txt");
+		BufferedReader read1 = new BufferedReader(file);
+		String txt = "";
+		String txt1 = read1.readLine();
+		int i = 0;
+
+		while (txt1 != null) {
+			txt += "\n" + txt1;
+			if (i != 0) {
+				linksinResults = new ArrayList<String>();
+				linksinResults.add((i + 1) + ". " + txt);
+			}
+			txt1 = read1.readLine();
+			i++;
+
+		}
+		System.out.println(linksinResults);
+	}
+
+	public boolean htmlLinksPattern(String lin) {
+		Pattern p = Pattern.compile(pattern);
+		Matcher m = p.matcher(lin);
+		boolean matches = m.find();
+		if (matches) {
+			linksInHTML.add(m.group(1));
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public Iterator<String> getLinks() throws FileNotFoundException, IOException {
+		String i = "";
+		Iterator<String> linkIterator = linksInHTML.iterator();
+		while (((Iterator<String>) linksInHTML).hasNext()) {
+			i+=(linkIterator.next());
+		}
+		return (Iterator<String>)i;
+	}
+
 	public static void main(String[] args) throws IOException {
-		LinkFinder a =new LinkFinder();
-//		 File f = new File("C:\\Users\\ZFleezy\\Documents\\Java1Assignments\\re");
-//    	 String[] fil = f.list();
-//    	 for(String s: fil){
-//    		 System.out.println(fil.toString());
-//    	 }
-//		FileReader file = new FileReader("C:\\Users\\ZFleezy\\Documents\\Java1Assignments\\re\\results.txt");
-// 		BufferedReader read = new BufferedReader(file);
-// 		String txt = "";
-// 		String txt1 = read.readLine();
-// 		while(txt1 != null){
-// 			txt+="\n"+txt1;
-// 			txt1=read.readLine();
-// 		}
-//		System.out.println(txt);
-		InputStream site = null;
+		LinkFinder a = new LinkFinder();
+				InputStream site = null;
 		site = new FileInputStream("neumonts.txt");
-		
-//		char c;
-//		int i;
 		System.out.println("working");
-		
-//		System.out.println(read);
-//		 while((i=site.read())!=-1)
-//         {
-//            // converts integer to character
-//            c=(char)i;
-//            
-//            // prints character
-//            System.out.print(c);
-//            if(site!=null){
-//            	site.close();
-//            }
-//         }
 		a.processPage(site);
-		for(int i = 0; i<linesInHTML.size();i++){
-			a.htmlLinksPattern(linesInHTML.get(i));
-			System.out.println((i+1)+". "+linksInHTML.get(i));
+		System.out.println("----------------------");
+		int x = 0;
+		for (int i = 0; i < linesInHTML.size(); i++) {
+			boolean tf = a.htmlLinksPattern(linesInHTML.get(i));
+			if (tf) {
+				System.out.println((x + 1) + ". " + linksInHTML.get(x));
+				x++;
+			}
 		}
 	}
 }
---
+++ SimpleLinkFinder/src/app/simpleLinkTester.java
@@ -0,0 +1,14 @@
+package app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class simpleLinkTester {
+	private LinkFinder test = new LinkFinder();
+	@Test
+	public void test() {
+		assertTrue(test.);
+	}
+
+}
